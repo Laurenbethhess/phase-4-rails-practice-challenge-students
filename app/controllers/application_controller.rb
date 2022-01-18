217214dfa4c26c2139516372be43a2f58001b057
Lauren Hess <laurenbethhess@gmail.com>
2022-01-18 09:39:39 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found 
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+def render_not_found
+    render json: { error: "Item not found" }, status: :not_found
+end
+
+def render_invalid(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+end
+
 end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,36 @@
+class StudentsController < ApplicationController
+
+    def index
+        render json: Student.all
+    end
+
+    def show
+        render json: find_student
+    end
+
+    def create
+        student= Student.create!(student_params)
+        render json: student, status: :created
+    end
+
+    def update
+        student = find_student
+        student.update(student_params)
+        render json: student
+    end
+
+    def destroy
+        find_student.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_student
+        Student.find(params[:id])
+    end
+
+    def student_params
+        params.permit(:name, :age, :instructor_id, :major)
+    end
+end
